b3e7747d1bcb56f940c027bef0798531144ef410
Crowe7 <jacobocrowe@gmail.com>
2022-05-05 17:55:42 -0500

    added poppins font to app

--- pages/api/hello.ts
+++ pages/api/hello.ts
@@ -1,4 +1,8 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+
+// gonna build my api without using next.js 
+// will leave this file  just in case
+
 import type { NextApiRequest, NextApiResponse } from 'next'
 
 type Data = {
