169b73c9ba06e7b7ef09a8f12ede753093052398
Crowe7 <jacobocrowe@gmail.com>
2022-05-05 20:47:07 -0500

    moved to parent dir

--- frontend-challenge/pages/_app.tsx
+++ frontend-challenge/pages/_app.tsx
@@ -20,8 +20,21 @@ export default function App(props: AppProps) {
           /** Put your mantine theme override here */
           fontFamily: 'Poppins',
           headings: {fontFamily: 'Poppins'},
-
-          colorScheme: 'light',
+          colorScheme: 'dark',
+          colors: {
+            dark: [
+              '#F5F5F5',
+              '#A6A7AB',
+              '#909296',
+              '#5C5F66',
+              '#373A40',
+              '#2C2E33',
+              '#25262B',
+              '#222222',
+              '#141517',
+              '#101113',
+            ],
+          },
         }}
       >
         <Component {...pageProps} />
@@ -29,3 +42,11 @@ export default function App(props: AppProps) {
     </>
   );
 }
+/*
+  Primary Colors:
+    Gold: #DEBF79
+    Dark Gray (background, text): #222222
+    Mid Gray (input text): #858585
+    Light Gray (inputs, contrast text): #F5F5F5
+    Red: #800000
+*/
