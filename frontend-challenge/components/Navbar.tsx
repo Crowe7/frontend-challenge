dc9a15ab90fef4eb0f1c81eb657e47828e68abe6
Crowe7 <jacobocrowe@gmail.com>
2022-05-07 03:27:25 -0500

    finished navbar

--- frontend-challenge/components/Navbar.tsx
+++ frontend-challenge/components/Navbar.tsx
@@ -1,32 +1,57 @@
 import React from 'react'
 import { NextRouter, useRouter } from 'next/router'
-import { Box, Button} from '@mantine/core';
-import Logo from '../public/assets/Logo.png'
+import { Box, Button, Image} from '@mantine/core';
 import Link from 'next/link';
-import Image from 'next/image';
+
 export const Navbar = () => {
     const router:NextRouter = useRouter();
-    
+
     let linkInfo: {name: string, href: string} = {
-        name: "Home",
+        name: "home",
         href: "/"
     }
     if(router.pathname !== '/') {
         linkInfo = {
-            name: "Home",
+            name: "home",
             href: "/"
         }
     } else {
         linkInfo = {
-            name: "Contact",
+            name: "contact",
             href: "/contact"
         }
     }
         return (
-            <Box sx={{display: "flex", justifyContent: "space-between", alignItems: "center"}}>
-                <Image src={Logo} alt="Company Logo"/>
+            <Box sx={{display: "flex",
+                    justifyContent: "space-between", 
+                    alignItems: "center",
+                    padding: 80,
+                    height: 130,
+                    '@media (max-width: 620px)': {padding: 30}
+                }}>
+                <Image
+                    src='https://raw.githubusercontent.com/Midwestern-Interactive/tech-challenge/master/assets/Logo.png'
+                    alt='Company logo'
+                    sx={{
+                        '@media (max-width: 620px)': {width: "70%"}
+                    }}
+                />
                 <Link href={linkInfo.href}>
-                    <Button variant="subtle" sx={{color: "#DEBF79"}} component="a">{linkInfo.name}</Button>
+                    <Button variant="subtle" 
+                        styles={(theme) => ({
+                            root: {
+                                color: "#DEBF79",
+                                fontSize: "1.1em",
+                                padding: 0,
+                                margin: 0,
+                                '&:hover': {
+                                    backgroundColor: 'inherit'
+                                }
+                            }
+                        })}
+                        component="a">
+                        {linkInfo.name}
+                    </Button>
                 </Link>
             </Box>
         )
