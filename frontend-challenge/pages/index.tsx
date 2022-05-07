42dc51fb3ccb9ebea0184821b244d77f09e6da12
Crowe7 <jacobocrowe@gmail.com>
2022-05-07 01:05:48 -0500

    typed out data fetched from backend and made contact page file

--- frontend-challenge/pages/index.tsx
+++ frontend-challenge/pages/index.tsx
@@ -1,12 +1,32 @@
-import type { NextPage } from 'next'
+import type { GetStaticProps, NextPage } from 'next'
 import Head from 'next/head'
 import Image from 'next/image'
+import Link from 'next/link'
 import styles from '../styles/Home.module.css'
+import { ContactItems, HomeItems } from '../types'
 
-const Home: NextPage = () => {
+const Home: NextPage<{home: HomeItems[]}> = ({ home }) => {
   return (
-    <div>Hello</div>
+    <div>
+      <Link href='/contact'>
+        <a>Contact</a>
+      </Link>
+    </div>
   )
 }
 
+export const getStaticProps:GetStaticProps = async () => {
+  const homeFetch: HomeItems[] = await fetch('http://localhost:8080/lorem/Home')
+    .then(res => res.json());
+
+  const contactFetch: ContactItems[]  = await fetch('http://localhost:8080/lorem/Contact')
+    .then(res => res.json());
+
+    return {
+      props: {
+        home: homeFetch,
+        contact: contactFetch,
+      },
+    }
+};
 export default Home
---
+++ frontend-challenge/types.ts
@@ -0,0 +1,13 @@
+export interface HomeItems {
+    name: string,
+    title: string,
+    paragraph: string,
+    imageUrl: string
+}
+
+export interface ContactItems {
+    name: string,
+    title: string,
+    paragraph: string,
+    paragraphTwo: string
+}
