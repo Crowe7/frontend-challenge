58c5e46699931770acd06e72a79b7c8df547209a
Crowe7 <jacobocrowe@gmail.com>
2022-05-07 02:18:50 -0500

    made navbar DRY

--- frontend-challenge/components/Navbar.tsx
+++ frontend-challenge/components/Navbar.tsx
@@ -6,29 +6,28 @@ import Link from 'next/link';
 import Image from 'next/image';
 export const Navbar = () => {
     const router:NextRouter = useRouter();
-    // This needs to be DRY instead of whatever this currently is..
+    
     let linkInfo: {name: string, href: string} = {
         name: "Home",
         href: "/"
     }
     if(router.pathname !== '/') {
-        return (
-            <Box sx={{display: "flex", justifyContent: "space-between", alignItems: "center"}}>
-                <Image src={Logo} alt="Company Logo"/>
-                <Link href='/'>
-                    <Button variant="subtle" sx={{color: "#DEBF79"}} component="a">Home</Button>
-                </Link>
-            </Box>
-        )
+        linkInfo = {
+            name: "Home",
+            href: "/"
+        }
     } else {
+        linkInfo = {
+            name: "Contact",
+            href: "/contact"
+        }
+    }
         return (
             <Box sx={{display: "flex", justifyContent: "space-between", alignItems: "center"}}>
                 <Image src={Logo} alt="Company Logo"/>
-                <Link href='/contact'>
-                    <Button variant="subtle" sx={{color: "#DEBF79"}} component="a">Contact</Button>
+                <Link href={linkInfo.href}>
+                    <Button variant="subtle" sx={{color: "#DEBF79"}} component="a">{linkInfo.name}</Button>
                 </Link>
             </Box>
         )
-    }
-
 }
