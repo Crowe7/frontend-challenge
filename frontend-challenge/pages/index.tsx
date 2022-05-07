767630c605eacdbb551cb184c3c4a7208b25df44
Crowe7 <jacobocrowe@gmail.com>
2022-05-07 06:58:38 -0500

    wrote todos for home page

--- frontend-challenge/pages/index.tsx
+++ frontend-challenge/pages/index.tsx
@@ -12,10 +12,22 @@ const Home: NextPage<{home: HomeItems[]}> = ({ home }) => {
     <div>
       <Navbar/>
       <LoremBoxes Boxes={home} />
+
     </div>
   )
 }
+// in state hold the 2 arrays in the readme and pass both of them into a function placed inside of the button we have to click
+// when button is clicked call the function in data and then use the notification provider to display that the operation is done?
+// gold list dots and light text
+// same padding as everything else of course.
 
+// TODO 
+// RESPONSIVE LOREM
+// WRITE HEADING ONE AND DUPLICATES TEXT
+// RESPONSIVE HEADING ONE AND DUPLICATES TEXT
+// FUNCTION TO COMBINE BOTH ARRYS INTO A SET THEN DISPLAY THEM
+// NOTIFICATIONS PROVIDER SUPPORT FOR COMBINE FUNCTION
+// DISPLAY ARRAY RESULTS
 export const getStaticProps:GetStaticProps = async () => {
   const homeFetch: HomeItems[] = await fetch('http://localhost:8080/lorem/Home')
     .then(res => res.json());
