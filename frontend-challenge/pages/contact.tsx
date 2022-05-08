152daee54ae6aecc855850298ce91bee5a7bd14d
Crowe7 <jacobocrowe@gmail.com>
2022-05-08 18:24:12 -0500

    changed div parent to a Box for ease of use with setting media query

--- frontend-challenge/pages/contact.tsx
+++ frontend-challenge/pages/contact.tsx
@@ -1,3 +1,4 @@
+import { Box } from '@mantine/core'
 import type { GetStaticProps, NextPage } from 'next'
 import Head from 'next/head'
 import Image from 'next/image'
@@ -9,12 +10,14 @@ import styles from '../styles/Home.module.css'
 import { ContactItems, HomeItems } from '../types'
 
 
-
 const contact: NextPage<{contact: ContactItems[]}> = ({contact}) => {
     return (
-        <div style={{background: "linear-gradient(to right, #222222 0%, #222222 50%, #ffffff 50%, #ffffff 100%)", height: "100vh"}}>
+        <Box sx={{background: "linear-gradient(to right, #222222 0%, #222222 50%, #ffffff 50%, #ffffff 100%)",
+                    height: "100vh",
+                    '@media (max-width: 1000px)': {background: "linear-gradient(#222222 0%, #222222 50%, #ffffff 50%, #ffffff 100%)"}
+                }}>
            <Navbar/>
-        </div>
+        </Box>
     )
 }
 
@@ -30,3 +33,29 @@ export const getStaticProps:GetStaticProps = async () => {
 };
 
 export default contact
+
+/*
+            <style global jsx> {`
+            
+                body {
+                    background: linear-gradient(
+                        to right, 
+                        #222222 0%, 
+                        #222222 50%, 
+                        #ffffff 50%, 
+                        #ffffff 100%
+                        );
+                    }
+                @media (max-width: 1000px) {
+                    body {
+                        background: linear-gradient(
+                            #222222 0%, 
+                            #222222 50%, 
+                            #ffffff 50%, 
+                            #ffffff 100%
+                        )
+                    }
+                }
+            `}</style>
+style={{background: "linear-gradient(to right, #222222 0%, #222222 50%, #ffffff 50%, #ffffff 100%)"}}
+*/
