ec8d17c978b5861e50fc0561263bd8e284b98929
Crowe7 <jacobocrowe@gmail.com>
2022-05-07 19:56:06 -0500

    made loremBoxes responsive

--- frontend-challenge/components/LoremBoxes.tsx
+++ frontend-challenge/components/LoremBoxes.tsx
@@ -11,11 +11,15 @@ export const LoremBoxes = ({Boxes}: MyProps) => {
   return (
     <Box sx={{
         display: "flex",
-        justifyContent: "center"
+        justifyContent: "center",
+        
     }}>
         <SimpleGrid 
             cols={3}
             spacing={'xl'}
+            breakpoints={[
+                {maxWidth: 1000, cols:1}
+            ]}
         >
             {Boxes.map((info) => {
                     return (
@@ -23,7 +27,11 @@ export const LoremBoxes = ({Boxes}: MyProps) => {
                             backgroundColor: "#F5F5F5",
                             height: 400,
                             width: 400,
-                            borderRadius: "3px"
+                            borderRadius: "3px",
+                            '@media (max-width: 1250px)': {width: 375, height: 375},
+                            '@media (max-width: 1170px)': {width: 350, height: 350},
+                            '@media (max-width: 1100px)': {width: 325, height: 325},
+                            '@media (max-width: 1000px)': {width: 400, height: 400}
                         }}>
                             <Box sx={{
                                 display: "flex",
@@ -32,7 +40,8 @@ export const LoremBoxes = ({Boxes}: MyProps) => {
                                 flexDirection: "column",
                                 paddingTop: 30,
                                 paddingBottom: 30,
-                                
+                                '@media (max-width: 1100px)': {paddingTop: 5},
+                                '@media (max-width: 1000px)': {paddingTop: 30}
                             }} >
                                 <Box sx={{
                                     display: "flex",
@@ -49,19 +58,30 @@ export const LoremBoxes = ({Boxes}: MyProps) => {
                                     gap: 15,
                                     paddingLeft: 55,
                                     paddingRight: 55,
-                                    paddingBottom: 25
+                                    paddingBottom: 25,
+                                    '@media (max-width: 1250px)': {paddingLeft: 35, paddingRight: 35, paddingBottom: 15, },
+                                    '@media (max-width: 1170px)': {paddingLeft: 35, paddingRight: 35, paddingBottom: 5, },
+                                    '@media (max-width: 1000px)': {paddingLeft: 55, paddingRight: 55, paddingBottom: 25, }
                                 }} >
                                     <Title sx={{color: "#222222"}} order={1}>{info.title}</Title>
 
-                                    <Text sx={{color: "#858585"}} weight='400' size='sm'>{info.paragraph}</Text>
+                                    <Text sx={{
+                                                color: "#858585", 
+                                                '@media (max-width: 1170px)': {fontSize: ".85rem" }, 
+                                                '@media (max-width: 1100px)': {fontSize: ".75rem" },
+                                                '@media (max-width: 1000px)': {fontSize: ".95rem" },
+
+                                            }} weight='400' size='sm'>
+                                        {info.paragraph}
+                                    </Text>
                                 </Box>
 
                                 <Button radius="xs" size="md"
+
                                     styles={(theme) => ({
                                         root: {
                                             fontSize: ".8rem",
                                             color: "#F5F5F5",
-
                                             backgroundColor: "#DEBF79",
                                             '&:hover': {
                                                 backgroundColor: theme.fn.darken('#DEBF79', 0.05),
